0162476a76bf60560bd4db9f647bfcc8c40f19c7
Dr Z <zamore@vt.edu>
2021-09-07 09:07:55 -0600

    For your assignment

--- PC03_start.py
+++ PC03_start.py
@@ -7,14 +7,13 @@ May 29, 2020
 
 PROGRAM DESCRIPTION GOES HERE <---------
 '''
-
-from turtle import * #import the library of commands that you'd like to use
+import turtle #import the library of commands that you'd like to use
 import math #get a bunch of math commands
 
-colormode(255)
+turtle.colormode(255)
 
 # Create a panel to draw on. 
-panel = Screen()
+panel = turtle.Screen()
 w = 750 # width of panel
 h = 750 # height of panel
 panel.setup(width=w, height=h) #600 x 600 is a decent size to work on. 
@@ -23,17 +22,14 @@ panel.setup(width=w, height=h) #600 x 600 is a decent size to work on.
 #=======Add your code here======
 
 panel.bgcolor("maroon") # set background color
-tracer(0) # turn off the animation  #DELETE.
+#turtle.tracer(0) # turn off the animation  #uncomment along with line 33. ALWAYS update the panel!
 
-Turtle(visible = False) # create a turtle, make it invisible 
-# BONUS: what kind of variable is "visible"?
+turtle.Turtle(visible=False) # create a turtle, make it invisible 
 
 #===================
 # YOUR CODE HERE!
 
 
-
+#panel.update() # if the animation is off (line 25 is uncommented), then uncomment this line.
 #===================
-
-
-# done()
+# turtle.done()
